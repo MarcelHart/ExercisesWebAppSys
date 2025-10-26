0697ab25abe2a57d09c6074a423ce51d88ab7840
MarcHrt <macimueller+gitHub@gmail.com>
2025-10-26 07:00:40 +0100

    started rough user interaction options framework

--- homework1/project/src/main/java/edu/fra/uas/homeworkApplication.java
+++ homework1/project/src/main/java/edu/fra/uas/homeworkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import edu.fra.uas.service.Studiengang;
 import edu.fra.uas.controller.NotenController;
 import edu.fra.uas.service.Kurs;
+import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +24,8 @@ public class HomeworkApplication {
 	NotenController notenController;
 	@Autowired
 	Studiengang wirtschaftsinformatikerBsc;
+	@Autowired
+	Scanner input;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkApplication.class, args);
@@ -54,6 +57,45 @@ public class HomeworkApplication {
 
 				log.debug("Homework Application started successfully");
 							
+				while (true) {
+					log.debug("Geben Sie 'exit' ein, um die Anwendung zu beenden.\n" +
+					"Kurs hinzufuegen: add \n" +
+					"Kurs entfernen: remove \n" +
+					"Kurse anzeigen: print \n" +
+					"Durchschnitt berechnen: average\n" +
+					"Note hinzufuegen: addnote \n" +
+					"Note entfernen: removenote \n" +
+					"Noten anzeigen: printnotes");
+
+					String command = input.nextLine();
+					if (command.equalsIgnoreCase("exit")) {
+						log.debug("Beenden der Anwendung...");
+						break;
+					} else if (command.equalsIgnoreCase("add")) {
+						// Code zum Hinzufügen eines Kurses
+						log.debug("Kurs hinzufuegen - Funktion noch nicht implementiert.");
+					} else if (command.equalsIgnoreCase("remove")) {
+						// Code zum Entfernen eines Kurses
+						log.debug("Kurs entfernen - Funktion noch nicht implementiert.");
+					} else if (command.equalsIgnoreCase("print")) {
+						wirtschaftsinformatikerBsc.printKurse();
+					} else if (command.equalsIgnoreCase("average")) {
+						double avg = wirtschaftsinformatikerBsc.berechneNotendurchschnitt();
+						log.debug("Notendurchschnitt: " + avg);
+					} else if (command.equalsIgnoreCase("addnote")) {
+						// Code zum Hinzufügen einer Note
+						log.debug("Note hinzufuegen - Funktion noch nicht implementiert.");
+					} else if (command.equalsIgnoreCase("removenote")) {
+						// Code zum Entfernen einer Note
+						log.debug("Note entfernen - Funktion noch nicht implementiert.");
+					} else if (command.equalsIgnoreCase("printnotes")) {
+						// Code zum Anzeigen der Noten
+						log.debug("Noten anzeigen - Funktion noch nicht implementiert.");
+					} 
+					else {
+						log.debug("Unbekannter Befehl: " + command);
+					}
+				}
 			}
 		};
 		return action;
