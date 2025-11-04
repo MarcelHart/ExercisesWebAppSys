bf4669720bf809d0cba2ec0dba97444747c91a58
MarcHrt <macimueller+gitHub@gmail.com>
2025-11-04 15:25:14 +0100

    implementing PostConstruct method to initialize courses and adding repository class for data storage.

---
+++ homework1/project/src/main/java/edu/fra/uas/config/initCourses.java
@@ -0,0 +1,33 @@
+package edu.fra.uas.config;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import edu.fra.uas.service.Kurs;
+
+@Component
+public class initCourses {
+
+    private final Logger log = org.slf4j.LoggerFactory.getLogger(InitData.class);
+
+    @Autowired
+    Kurs initKurs;
+
+
+    @PostConstruct
+    public void init() {
+        log.debug("### Initialize Courses ###");
+
+        log.debug("create course Software Engineering");
+        initKurs = new Kurs("Software Engineering", 101, 3);
+        initKurs.addNote(1, 90.0);
+        initKurs.addNote(2, 80.0);
+        log.debug("Course created: " + initKurs.toString());
+        log.debug("Grades:\n" + initKurs.notenAusgeben());
+
+        log.debug("### Courses initialized ###");
+    }
+
+}
---
+++ homework1/project/src/main/java/edu/fra/uas/repository/repositoryData.java
@@ -0,0 +1,12 @@
+package edu.fra.uas.repository;
+
+import org.springframework.stereotype.Repository;
+
+import edu.fra.uas.service.Studiengang;
+
+import java.util.HashMap;
+
+@Repository
+public class repositoryData extends HashMap<Integer, Studiengang> {
+
+}
