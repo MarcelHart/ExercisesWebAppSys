6b763f740c9f32d8203c8c4118e8b368510da109
MarcHrt <macimueller+gitHub@gmail.com>
2025-11-09 09:53:16 +0100

    moved init logic to initCourses method

--- homework1/project/src/main/java/edu/fra/uas/config/initCourses.java
+++ homework1/project/src/main/java/edu/fra/uas/config/initCourses.java
@@ -2,32 +2,37 @@ package edu.fra.uas.config;
 
 import javax.annotation.PostConstruct;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import edu.fra.uas.service.Studiengang;
 import edu.fra.uas.service.Kurs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class initCourses {
 
-    private static final Logger log = org.slf4j.LoggerFactory.getLogger(initCourses.class);
-    
-    @Autowired
-    Kurs initKurs;
+    private static final Logger log = LoggerFactory.getLogger(initCourses.class);
 
+    @Autowired
+    private Studiengang studiengang;
 
     @PostConstruct
     public void init() {
-        log.debug("### Initialize Courses ###");
+        if (studiengang.getKurse() == null || studiengang.getKurse().isEmpty()) {
+            log.debug("initCourses @PostConstruct: populating default courses");
+            Kurs winfo = new Kurs("Wirtschaftsinformatik", 301, 3);
+            winfo.addNote(1.3, 2);
+            winfo.addNote(2.0, 3);
+            studiengang.addKurs(winfo);
 
-        log.debug("create course Software Engineering");
-        initKurs = new Kurs("Software Engineering", 101, 3);
-        initKurs.addNote(1.0, 2);
-        initKurs.addNote(2.3, 4);
-        log.debug("Course created: " + initKurs.toString());
-        log.debug("Grades:\n" + initKurs.notenAusgeben());
-
-        log.debug("### Courses initialized ###");
+            Kurs algebra = new Kurs("Algebra", 201, 2);
+            algebra.addNote(2.3, 1);
+            algebra.addNote(4.7, 2);
+            studiengang.addKurs(algebra);
+        } else {
+            log.debug("initCourses @PostConstruct: studiengang already initialized, skipping");
+        }
     }
-
 }
--- homework1/project/src/main/java/edu/fra/uas/homeworkApplication.java
+++ homework1/project/src/main/java/edu/fra/uas/homeworkApplication.java
@@ -1,106 +1,43 @@
 package edu.fra.uas;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import edu.fra.uas.service.Studiengang;
-import edu.fra.uas.controller.NotenController;
-import edu.fra.uas.service.Kurs;
-import java.util.Scanner;
+import org.springframework.boot.CommandLineRunner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
-
+import edu.fra.uas.service.Studiengang;
+import edu.fra.uas.service.Kurs;
 
 @SpringBootApplication
 public class HomeworkApplication {
 
     private static final Logger log = LoggerFactory.getLogger(HomeworkApplication.class);
 
-    @Autowired
-    NotenController notenController;
-    @Autowired
-    Studiengang wirtschaftsinformatikerBsc;
-    @Autowired
-    Scanner input;  // Using a scanner Bean from configuration (normal Scanner can not be used by Spring)
-
     public static void main(String[] args) {
         SpringApplication.run(HomeworkApplication.class, args);
     }
 
-
     @Bean
-    CommandLineRunner init() {
-        CommandLineRunner action = new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-                
-                log.debug("Starting Homework Application");
-                    Kurs winfo = new Kurs("Wirtschaftsinformatik", 301, 3);
-                    winfo.addNote(1.3, 2);
-                    winfo.addNote(2.0, 3);
+    public CommandLineRunner runner(Studiengang studiengang) {
+        return args -> {
+            // guard to avoid duplicate initialization if initCourses already ran
+            if (studiengang.getKurse() == null || studiengang.getKurse().isEmpty()) {
+                log.debug("CommandLineRunner: populating sample courses");
+                Kurs winfo = new Kurs("Wirtschaftsinformatik", 301, 3);
+                winfo.addNote(1.3, 2);
+                winfo.addNote(2.0, 3);
+                studiengang.addKurs(winfo);
 
-                    Kurs algebra = new Kurs("Algebra", 201, 2);
-                    algebra.addNote(2.3, 1);
-                    algebra.addNote(4.7, 2);
-
-                    wirtschaftsinformatikerBsc.addKurs(winfo);
-                    wirtschaftsinformatikerBsc.addKurs(algebra);
-                    wirtschaftsinformatikerBsc.printKurse();
-
-                    double durchschnitt = wirtschaftsinformatikerBsc.berechneNotendurchschnitt();
-                    log.debug("Notendurchschnitt im Studiengang: " + durchschnitt);
-                
-
-                log.debug("Homework Application started successfully");
-                            
-                while (true) {
-                    log.debug("Geben Sie 'exit' ein, um die Anwendung zu beenden.\n" +
-                    "Kurs hinzufuegen: add \n" +
-                    "Kurs entfernen: remove \n" +
-                    "Kurse anzeigen: print \n" +
-                    "Durchschnitt berechnen: average\n" +
-                    "Note hinzufuegen: addnote \n" +
-                    "Note entfernen: removenote \n" +
-                    "Noten anzeigen: printnotes");
-
-                    String command = input.nextLine();
-                    if (command.equalsIgnoreCase("exit")) {
-                        log.debug("Beenden der Anwendung...");
-                        break;
-                    } else if (command.equalsIgnoreCase("add")) {
-                        // Code zum Hinzufügen eines Kurses
-                        log.debug("Kurs hinzufuegen - Funktion noch nicht implementiert.");
-                    } else if (command.equalsIgnoreCase("remove")) {
-                        // Code zum Entfernen eines Kurses
-                        log.debug("Kurs entfernen - Funktion noch nicht implementiert.");
-                    } else if (command.equalsIgnoreCase("print")) {
-                        wirtschaftsinformatikerBsc.printKurse();
-                    } else if (command.equalsIgnoreCase("average")) {
-                        double avg = wirtschaftsinformatikerBsc.berechneNotendurchschnitt();
-                        log.debug("Notendurchschnitt: " + avg);
-                    } else if (command.equalsIgnoreCase("addnote")) {
-                        // Code zum Hinzufügen einer Note
-                        log.debug("Note hinzufuegen - Funktion noch nicht implementiert.");
-                    } else if (command.equalsIgnoreCase("removenote")) {
-                        // Code zum Entfernen einer Note
-                        log.debug("Note entfernen - Funktion noch nicht implementiert.");
-                    } else if (command.equalsIgnoreCase("printnotes")) {
-                        // Code zum Anzeigen der Noten
-                        log.debug("Noten anzeigen - Funktion noch nicht implementiert.");
-                    } 
-                    else {
-                        log.debug("Unbekannter Befehl: " + command);
-                    }
-                }
+                Kurs algebra = new Kurs("Algebra", 201, 2);
+                algebra.addNote(2.3, 1);
+                algebra.addNote(4.7, 2);
+                studiengang.addKurs(algebra);
+            } else {
+                log.debug("CommandLineRunner: courses already present, skipping population");
             }
         };
-        return action;
     }
-    
-
-
 }
