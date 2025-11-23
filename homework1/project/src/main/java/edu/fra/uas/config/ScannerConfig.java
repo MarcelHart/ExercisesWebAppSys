4a112f2e37964302739ea9d11938c0f47a2cab06
MarcHrt <macimueller+gitHub@gmail.com>
2025-11-23 09:51:20 +0100

    troubleshooting startup process

--- homework1/project/src/main/java/edu/fra/uas/config/ScannerConfig.java
+++ homework1/project/src/main/java/edu/fra/uas/config/ScannerConfig.java
@@ -4,17 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.annotation.PreDestroy;
 import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class ScannerConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(ScannerConfig.class);
     
+    private Scanner scannerInstance;
+
     @Bean
     public Scanner scanner() {
-        return new Scanner(System.in);
+        scannerInstance = new Scanner(System.in);
+        return scannerInstance;
     }
 
-    @PreDestroy // Close the scanner when the application context is destroyed
+    @PreDestroy
     public void closeScanner() {
-        scanner().close();
+        if (scannerInstance != null) {
+            log.debug("Closing Scanner bean");
+            scannerInstance.close();
+        }
     }
 }
